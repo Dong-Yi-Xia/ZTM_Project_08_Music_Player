3102d028f7850a2519806f3eaeeba5f568422bcb
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-02-22 21:25:56 -0500

    event listner

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const image = document.querySelector('img')
 const title = document.getElementById('title')
 const artist = document.getElementById('artist')
 
-
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
+
+const currentTimeEl = document.getElementById('current-time')
+const durationEl = document.getElementById('duration')
+
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
@@ -42,7 +47,7 @@ function playSong(){
     isPlaying = true
     playBtn.classList.replace('fa-play', 'fa-pause')
     playBtn.setAttribute('title', 'Pause')
-    music.play()
+    music.play()  //audio and video methods
 }
 
 // Pause 
@@ -50,7 +55,7 @@ function pauseSong(){
     isPlaying = false
     playBtn.classList.replace('fa-pause', 'fa-play')
     playBtn.setAttribute('title', 'Play')
-    music.pause()
+    music.pause() //audio and video methods
 }
 
 // Play or Pause Event Listener
@@ -96,6 +101,54 @@ function nextSong(){
 loadSong(songs[songIndex])
 
 
+// Update Progress Bar and Time
+function updateProgressBar(evt){
+    if(isPlaying){
+        const {duration, currentTime} = evt.srcElement  // destructuring on the music element 
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100
+        progress.style.width = `${progressPercent}%`
+
+        // Calculate display for duration
+        const durationMinutes = Math.floor( duration / 60)
+        let durationSeconds = Math.floor( duration % 60)
+        if(durationSeconds < 10){
+            durationSeconds = `0${durationSeconds}`
+        }
+     
+        // Delay switching duration Element to avoid NaN
+        if(durationSeconds){
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`
+        }
+
+         // Calculate display for current duration
+         const currentMinutes = Math.floor( currentTime / 60)
+         let currentSeconds = Math.floor( currentTime % 60)
+         if(currentSeconds < 10){
+             currentSeconds = `0${currentSeconds}`
+         }
+         currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
+    }
+}
+
+// Set Progress Bar
+function setProgressBar(evt){
+    // evt.srcElement points to the current element, can use THIS to point to itself
+    const width = this.clientWidth
+    const clickX = evt.offsetX
+    const {duration} = music  // music === evt.srcElement 
+    console.log(duration)
+    console.log(clickX/width)
+    console.log( (clickX/width) * duration )
+    music.currentTime = (clickX/width) * duration  //audio and video Property
+
+}
+
+
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+music.addEventListener('endeed', nextSong)
+music.addEventListener('timeupdate', updateProgressBar) //audio and video Event Listener
+progressContainer.addEventListener('click', setProgressBar)
